4b252177f66d8506990b3d805eaa06ed367879a5
ivan <ivanandresmm1996@gmail.com>
2018-10-21 12:00:08 -0500

    Agregadas Tablas: Departamento, Ciudad y Genero

--- app/Ciudad.php
+++ app/Ciudad.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Departamento extends Model
+class Ciudad extends Model
 {
     //
 }
--- app/Genero.php
+++ app/Genero.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Departamento extends Model
+class Genero extends Model
 {
     //
 }
