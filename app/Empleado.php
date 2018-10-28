72ece9f9bb38445781b490844c1e3ce8fe0c8820
Andres.Gustin <andres.gustin@correounivalle.edu.co>
2018-10-28 10:53:00 -0500

    app empleado

--- app/Empleado.php
+++ app/Empleado.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empleado extends Model
 {
-<<<<<<< HEAD
     protected $fillable = ['id','name','apellido','telefono','correoelectronico','id_tipovinculacion','fechadenacimiento','salario','id_cargo','id_sede','fechadeingreso'];
-=======
-    protected $fillable = ['id','name','apellido','telefono','correoelectronico','id_tipovinculacion','fechadenacimiento','salario','id_cargo','id_sede'];
->>>>>>> 9219f0e8ea782515b8897b6899fa272b3fe9d2d7
 }
