a908dc69d5f7fb0194f3ae7c0bf7632d78d305bc
Andres.Gustin <andres.gustin@correounivalle.edu.co>
2019-02-10 15:54:07 -0500

    Edit Ausentismo

--- resources/views/ausentismos/edit.blade.php
+++ resources/views/ausentismos/edit.blade.php
@@ -33,7 +33,7 @@
                                 <div class="col-xs-6 col-sm-6 col-md-6">
                                     <div class="form-group">
                                     <input type="text" name="fecha_registro" value="{{$ausentismo->fecha_registro}}" id="fecha" class="form-control" >
-                                    <input type="button" class="btn btn-primary" value="Buscar" onclick="dateCompare();">        
+                                    <!-- <input type="button" class="btn btn-primary" value="Buscar" onclick="dateCompare();">         -->
                             <input type="text" name="obsevacion" value="{{$ausentismo->observacion}}" id="cargo_title" class="form-control" >
                                     </div>
                                 </div>
@@ -56,26 +56,37 @@
             <a href="#" class="btn btn-info" id="botonProrroga" >Anadir Prorroga</a>
             <script>
      $(document).ready(function() {
-    var fecha_aux = document.getElementById("fecha").value.split("-");
+        var fecha_aux = document.getElementById("fecha").value.split("-");
     var Fecha1 = new Date(parseInt(fecha_aux[0]),parseInt(fecha_aux[1]-1),parseInt(fecha_aux[2]));
+    //  console.log(Fecha1.setDate(Fecha1.getDate + 5));
      Hoy = new Date();//Fecha actual del sistema
+
+     var diasAnadir = 5;
+
      var AnyoFecha = Fecha1.getFullYear();
-     var MesFecha = Fecha1.getMonth();
-     var DiaFecha = Fecha1.getDate();
+     var MesFecha = Fecha1.getMonth() + 1;
+     var DiaFecha = Fecha1.getDate() + diasAnadir;
+     
+     var Fecha2 = AnyoFecha+"-"+MesFecha+"-"+DiaFecha; 
+    //  var Fecha3 = Date.parse(Fecha2);
+    //  var Fecha4 = new Date(Fecha3);
+    document.getElementById('cargo_title').value=Fecha2;
+    console.log(Fecha2);
+     
       
      var AnyoHoy = Hoy.getFullYear();
      var MesHoy = Hoy.getMonth();
      var DiaHoy = Hoy.getDate();
 
-                 if (AnyoFecha >= AnyoHoy && MesFecha >= MesHoy && DiaFecha >= DiaHoy){
-                    // $('#botonProrroga').prop('disabled', false);
-                    alert('Activar Boton');
-                 }
-                 else{
-                    // $('#botonProrroga').prop('disabled', true);
-                    alert('Desactivar Boton');
-                 }
-})
+                  if (AnyoFecha >= AnyoHoy && MesFecha >= MesHoy && DiaFecha >= DiaHoy){
+                      $('#botonProrroga').prop('disabled', false);
+                     alert('Activar Boton');
+                  }
+                  else{
+                      $('#botonProrroga').prop('disabled', true);
+                     alert('Desactivar Boton');
+                  }
+    })
             </script>
         </section>
 @endsection
